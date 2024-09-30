a3c5dd9c58379c2bbd1c822129f5c95221d25ccb
Mike Lappas <lappas.m@northeastern.edu>
2024-09-30 17:47:15 -0400

    Correct the styling of the 'Course Status' screen

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <div className="flex-fill">
                 <Modules />
             </div>
-            <div className="d-none d-md-block">
+            <div className="d-none d-xl-block">
                 <CourseStatus />
             </div>
         </div>
